fe321cd09faa3d8fd1efe834af73565a7ef61e8c
ibad-cr <smashingfour.1888@gmail.com>
2024-08-08 18:49:55 +0400

    FilterButton Shop Responsive

--- src/components/ShopComponents/ShopSingleCard.jsx
+++ src/components/ShopComponents/ShopSingleCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const ShopSingleCard = ({ id, image, title, text, category, price, quantity, alldata }) => {
     return (
-        <div className=''>
+        <div className='shop-single-card'>
             <div class="col">
                 <div class="card">
-                    <img src={image} class="card-img-top" alt="..." style={{width: '100%', height: '300px' , objectFit: 'cover'}} />
+                    <img src={image} class="card-img-top" alt="..."  />
                     <div class="card-body">
                         <h5 class="card-title">{title}</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">This is a longer card with supporting text.</p>
                     </div>
                 </div>
             </div>
--- src/page/Shop.jsx
+++ src/page/Shop.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import ShopSingleCard from '../components/ShopComponents/ShopSingleCard';
-import ShopLeftSide from '../components/ShopComponents/ShopLeftSIde';
+import { RxCaretSort } from 'react-icons/rx';
 
 const Shop = () => {
     const shop = useSelector(state => state.shop);
@@ -13,34 +13,154 @@ const Shop = () => {
     }, [shop]);
 
 
+    // ===========================Drop-Down-Menu=========================================
+
+    const [openDropdownIndex, setOpenDropdownIndex] = useState(null);
+    const dropdownRefs = useRef([]);
+
+    const toggleDropdown = (index) => {
+        setOpenDropdownIndex(openDropdownIndex === index ? null : index);
+    };
+
+    const handleClickOutside = (event) => {
+        if (dropdownRefs.current.every(ref => ref && !ref.contains(event.target))) {
+            setOpenDropdownIndex(null);
+        }
+    };
+
     useEffect(() => {
-        console.log('FilterShop data:', filterShop);
-    }, [filterShop]);
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    // ==========================Filter-Buttons============================================
+    const filterData = (cat) => {
+        if (cat === 'All') {
+            setFilterShop(shop);
+        } else if (cat === 'Free') {
+            const result = shop.filter(item => item.price === 0);
+            setFilterShop(result);
+        } else if (cat === 'Paid') {
+            const result = shop.filter(item => item.price > 0);
+            setFilterShop(result);
+        }
+    };
+
 
     return (
         <div className="shop-container container mt-5">
-            <div className='row'>
-                <div className='col-12 col-sm-12 col-md-3' style={{ width: '50% !important' }}>
-                    <ShopLeftSide />
-                </div>
-                <div className='col-12 col-sm-12 col-md-9'>
-                    <div>
-                        <div className='row row-cols-2 row-cols-md-3 g-4'>
-                            {filterShop.map((item, index) => (
-                                <div className='' key={index}>
-                                    <ShopSingleCard
-                                        image={item.image}
-                                        title={item.title}
-                                        text={item.text}
-                                        category={item.category}
-                                        price={item.price}
-                                        quantity={item.quantity}
-                                        alldata={item}
-                                    />
+            <div>
+                <div className='d-flex align-items-center justify-content-between mb-3'>
+                    <div className='dropdown-button-list'>
+                        <ul className='all-button-list'>
+                            <li className='list-group-item'>
+                                <div className="dropdown" ref={el => dropdownRefs.current[0] = el}>
+                                    <button className="dropdown-btn" onClick={() => toggleDropdown(0)}>
+                                        Price
+                                    </button>
+                                    <div className={`dropdown-content ${openDropdownIndex === 0 ? 'open' : ''}`}>
+                                        <ul className='price-list mt-3'>
+                                            <li className='list-group-item'>
+                                                <button onClick={() => { setOpenDropdownIndex(null); filterData('All') }}>All</button>
+                                            </li>
+                                            <li className='list-group-item'>
+                                                <button onClick={() => { setOpenDropdownIndex(null); filterData('Free') }}>Free</button>
+                                            </li>
+                                            <li className='list-group-item'>
+                                                <button onClick={() => setOpenDropdownIndex(null)}>Paid</button>
+                                            </li>
+                                            <li className='list-group-item'>
+                                                <button onClick={() => setOpenDropdownIndex(null)}>Discount</button>
+                                            </li>
+                                        </ul>
+                                    </div>
                                 </div>
-                            ))}
+                            </li>
+                            <li className='list-group-item'>
+                                <div className="dropdown" ref={el => dropdownRefs.current[1] = el}>
+                                    <button className="dropdown-btn" onClick={() => toggleDropdown(1)}>
+                                        Color
+                                    </button>
+                                    <div className={`dropdown-content ${openDropdownIndex === 1 ? 'open' : ''}`}>
+                                        <ul className='color-list mt-3'>
+                                            <div className='first-part'>
+                                                <li className='list-group-item'>
+                                                    <button className='red' onClick={() => setOpenDropdownIndex(null)}></button>
+                                                </li>
+                                                <li className='list-group-item'>
+                                                    <button className='green' onClick={() => setOpenDropdownIndex(null)}></button>
+                                                </li>
+                                                <li className='list-group-item'>
+                                                    <button className='yellow' onClick={() => setOpenDropdownIndex(null)}></button>
+                                                </li>
+                                            </div>
+                                            <div className='secont-part'>
+                                                <li className='list-group-item'>
+                                                    <button className='blue' onClick={() => setOpenDropdownIndex(null)}></button>
+                                                </li>
+                                                <li className='list-group-item'>
+                                                    <button className='purple' onClick={() => setOpenDropdownIndex(null)}></button>
+                                                </li>
+                                                <li className='list-group-item'>
+                                                    <button className='fix-color' onClick={() => setOpenDropdownIndex(null)}></button>
+                                                </li>
+                                            </div>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className='list-group-item'>
+                                <div className="dropdown" ref={el => dropdownRefs.current[2] = el}>
+                                    <button className="dropdown-btn" onClick={() => toggleDropdown(2)}>
+                                        Brand
+                                    </button>
+                                    <div className={`dropdown-content ${openDropdownIndex === 2 ? 'open' : ''}`}>
+                                        <ul>
+                                            <li className='list-group-item'>
+                                                <button onClick={() => setOpenDropdownIndex(null)}>Brand A</button>
+                                                <button onClick={() => setOpenDropdownIndex(null)}>Brand B</button>
+                                                <button onClick={() => setOpenDropdownIndex(null)}>Brand C</button>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className='sort-method' ref={el => dropdownRefs.current[3] = el}>
+                        <button className="sort-dropdown-btn" onClick={() => toggleDropdown(3)}>
+                            <RxCaretSort />
+                        </button>
+                        <div className={`sort-dropdown-content ${openDropdownIndex === 3 ? 'open' : ''}`}>
+                            <ul className='sort-list mt-3' style={{ padding: '0', margin: '0' }}>
+                                <li className='list-group-item'>
+                                    <button>Price: low to high</button>
+                                    <button>Price: low to high</button>
+                                </li>
+                            </ul>
                         </div>
                     </div>
+                </div>
+
+                {/* <div className='middle-line'></div> */}
+
+                <div className='row row-cols-2 row-cols-md-4 g-4'>
+                    {filterShop.map((item, index) => (
+                        <div className='' key={index}>
+                            <ShopSingleCard
+                                image={item.image}
+                                title={item.title}
+                                text={item.text}
+                                category={item.category}
+                                price={item.price}
+                                quantity={item.quantity}
+                                alldata={item}
+                            />
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
